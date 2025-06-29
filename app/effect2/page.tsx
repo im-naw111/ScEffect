3f855b4436b1e76329f3003251bca62758d79292
Imam Nawawi <44057098+im-naw111@users.noreply.github.com>
2025-06-29 23:51:01 +0700

    Update page.tsx

--- app/effect2/page.tsx
+++ app/effect2/page.tsx
@@ -145,7 +145,7 @@ const Html = () => {
             }}
           >
             <div className="flex space-x-2 lg:space-x-6 overflow-hidden">
-              {'Nawawow'.split(' ').map((word, index) => {
+              {'imam nawawi'.split(' ').map((word, index) => {
                 return (
                   <div data-title key={index}>
                     {word}
@@ -157,7 +157,8 @@ const Html = () => {
 
           <div className=" text-center text-xs md:text-xl xl:text-2xl 2xl:text-3xl mt-2 overflow-hidden">
             <div data-desc>
-              <div>Welcome to my site :V</div>
+              <br>
+              
               <div>www.nawawow.site</div>
             </div>
           </div>
