c460bba4b0b7d9e4dd78efb6a572041a128280af
Imam Nawawi <44057098+im-naw111@users.noreply.github.com>
2025-06-30 18:50:34 +0700

    Update page.tsx

--- app/effect2/page.tsx
+++ app/effect2/page.tsx
@@ -157,7 +157,7 @@ const Html = () => {
 
           <div className=" text-center text-xs md:text-xl xl:text-2xl 2xl:text-3xl mt-2 overflow-hidden">
             <div data-desc>
-              <div>was here</div>
+              <div>besenang senanglah dalam dunia maya dan dunia nyata kita butuh hiburan bukan tekanan hidup hanya sekali bersenang senanglah dengan waktu yang ada tapi ingat jika ada kehidupan pasti ada kematian dan sama halnya perbuatan pasti ada balasan</div>
                 </div>
           </div>
         </div>
