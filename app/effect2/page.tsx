347442a257e47a4557ddc1c23d5b7ed6628b92f8
Imam Nawawi <44057098+im-naw111@users.noreply.github.com>
2025-06-30 07:01:11 +0700

    Update page.tsx

--- app/effect2/page.tsx
+++ app/effect2/page.tsx
@@ -157,8 +157,8 @@ const Html = () => {
 
           <div className=" text-center text-xs md:text-xl xl:text-2xl 2xl:text-3xl mt-2 overflow-hidden">
             <div data-desc>
-              <div>baris1</div><br/>
-              <div>baris2</div><br/>
+              <div>baris1</div>
+              <div>baris2</div>
               <div>www.nawawow.site</div>
             </div>
           </div>
