ad83c5c4f1383f46972e25fa01e8d7ec740b872b
Imam Nawawi <44057098+im-naw111@users.noreply.github.com>
2025-08-09 06:30:50 +0700

    Update page.tsx

--- app/id/page.tsx
+++ app/id/page.tsx
@@ -145,7 +145,7 @@ const Html = () => {
             }}
           >
             <div className="flex space-x-2 lg:space-x-6 overflow-hidden">
-              {'nawawow site'.split(' ').map((word, index) => {
+              {'zero nawawow'.split(' ').map((word, index) => {
                 return (
                   <div data-title key={index}>
                     {word}
