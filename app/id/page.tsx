05a3205fede8f1b8be90f27abe3a8cf019af3538
Imam Nawawi <44057098+im-naw111@users.noreply.github.com>
2026-01-03 21:18:46 +0700

    Replace img tag with Next.js Image component
    Refactor image rendering to use Next.js Image component for better optimization.

--- app/id/page.tsx
+++ app/id/page.tsx
@@ -134,7 +134,25 @@ const Html = () => {
         className="h-svh fixed z-90 bg-black pointer-events-none w-full flex justify-center items-center"
         data-loader
       >
-         <div className="wrapper"><img className="move" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/DVD-Video_Logo.svg/512px-DVD-Video_Logo.svg.png"/></div>     
+         <div className="wrapper">
+           <img className="move" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/DVD-Video_Logo.svg/512px-DVD-Video_Logo.svg.png"/>
+        
+         import Image from 'next/image'
+ 
+function Home() {
+  return (
+    <Image
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/DVD-Video_Logo.svg/512px-DVD-Video_Logo.svg.png"
+      alt="Landscape picture"
+      width={800}
+      height={500}
+    />
+  )
+}
+ 
+export default Home
+           
+         </div>     
       </div>
       <div className="h-svh">
         <div className="h-svh uppercase items-center w-full absolute z-60 pointer-events-none px-10 flex justify-center flex-col">
